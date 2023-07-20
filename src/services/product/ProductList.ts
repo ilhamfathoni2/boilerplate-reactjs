cf94828d9f251db14ac27b2ed89a7f9fd56058a2
ilham.fathoni <ilham.fathoni>
2023-07-20 11:47:51 +0700

    API create pproduct

--- src/services/product/ProductList.ts
+++ src/services/product/ProductList.ts
@@ -6,11 +6,60 @@ interface ProductListResponse extends AxiosResponse {
   data: ProductType;
 }
 
+// Get List Product
 export const getListProducts = async () => {
   try {
     const res: AxiosResponse = await client<ProductListResponse>({
       url: "/api/v1/ProductList",
       method: "get",
+    });
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return error?.response ?? null;
+    } else {
+      return null;
+    }
+  }
+};
+
+// Create Product
+export const createProducts = async ({
+  name,
+  href,
+  imageSrc,
+  imageAlt,
+  price,
+  color,
+  size,
+  inStock,
+  leadTime,
+}: {
+  name: string;
+  href?: string;
+  imageSrc: string;
+  imageAlt: string;
+  price: number;
+  color: string;
+  size: string;
+  inStock: boolean;
+  leadTime?: string;
+}) => {
+  try {
+    const res: AxiosResponse = await client<ProductListResponse>({
+      url: "/api/v1/ProductList",
+      method: "post",
+      data: {
+        name,
+        href,
+        imageSrc,
+        imageAlt,
+        price,
+        color,
+        size,
+        inStock,
+        leadTime,
+      },
     });
     return res;
   } catch (error) {
