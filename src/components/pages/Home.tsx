5b7cb4d7c7264a62787df9fd61f02cec8d742a10
ilham.fathoni <ilham.fathoni>
2023-07-22 17:48:01 +0700

    metadata

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,24 +1,28 @@
 import { HeroSection } from "../home/Hero";
 import { Cta } from "../home/Cta";
 import { Footer } from "../footer/footer";
-
 import { Navbar } from "../navbar/Navbar";
 
 export default function HomePage() {
   return (
     <>
-      <meta name="title" content="Dress"></meta>
-      <meta
-        name="description"
-        content="This is a React.Js boilerplate that already provides various features including Integration API (CRUD), Redux, Redux Persist, switching .env when running the application, and using tailwindcss and daisyUi."
-      ></meta>
-      <meta
-        name="keywords"
-        content="boilerplate, reactjs, react.js, tailwindcss, daisyUi"
-      ></meta>
-      <meta name="robots" content="noindex, nofollow"></meta>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
-      <meta name="language" content="English"></meta>
+      <head>
+        <meta name="title" content="Dress.id"></meta>
+        <meta
+          name="description"
+          content="This is a React.Js boilerplate that already provides various features including Integration API (CRUD), Redux, Redux Persist, switching .env when running the application, and using tailwindcss and daisyUi."
+        ></meta>
+        <meta
+          name="keywords"
+          content="boilerplate, reactjs, react.js, tailwindcss, daisyUi"
+        ></meta>
+        <meta name="robots" content="noindex, nofollow"></meta>
+        <meta
+          http-equiv="Content-Type"
+          content="text/html; charset=utf-8"
+        ></meta>
+        <meta name="language" content="English"></meta>
+      </head>
 
       <Navbar />
       <HeroSection />
